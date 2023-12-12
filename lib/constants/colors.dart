da976e2974f3b70df68b3ecb99388a421455df07
Harshrai1023 <harshrai1023@gmail.com>
2023-12-12 20:31:16 +0530

    color palette

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color cusRed = Colors.red.shade400;
+Color cusBlue = Colors.blue.shade600;
+Color cusBlack = Colors.black;
+Color cusGrey = Colors.grey;
+Color cusBGColor = Colors.blue.shade50;
